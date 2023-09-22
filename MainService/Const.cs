e11aeba2869c90ff549d3902d99c3a6dc55f3334
Planoff Dima <dimaplanoff@hotmail.com>
2023-09-22 13:23:38 +0300

    --

---
+++ MainService/Const.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SLLibrary;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Text.Json;
+
+
+namespace UniApp
+{
+    public class JConst
+    {
+        public string DbConnectionString { get; set; }      
+    }
+
+    public class Const 
+    {
+        public static bool IsDebug
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+
+        private Const()
+        { 
+        }
+
+        private static Const _this = null;
+        private static object sync = new ();        
+        public static Const Entity
+        {
+            get 
+            {
+                lock (sync)
+                {
+                    if(_this == null)
+                        _this = new Const();
+                    return _this;
+                }
+            }
+        }
+
+        public void Init(IConfiguration config)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            J = config.Get<JConst>();            
+        }
+
+        public static JConst J { get; private set; }
+        public static readonly JsonSerializerOptions JsonSerializerWeb = LibHelper.JsonSerializerWeb;
+
+    }
+}
---
+++ MainService/Model.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.Json;
+
+
+namespace UniApp
+{
+    public class Model
+    {
+        public class CPost 
+        {
+            public string Name { get; set; }
+            public JsonElement? Value { get; set; }
+
+            public object Write(Encoding encoding = null)
+            {
+                List<object> args = new();
+                string authParams = string.Empty;
+
+                if (Value != null && Value?.ValueKind != JsonValueKind.Null)
+                {
+                    var pName = $"@p_json";
+                    authParams += $",{pName}";
+                    args.Add(Value);
+                }
+
+                if (authParams.Length > 0)
+                    authParams = authParams.Substring(1);
+
+                using (SLLibrary.SQL.Context context = new(Const.J.DbConnectionString))
+                    return context.Execute<JsonElement>(null, $@"select ""{Name}""({authParams})", args);
+            }
+        }
+    }
+}
